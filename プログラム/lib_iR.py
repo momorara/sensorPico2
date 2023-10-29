1b29076c7eea7162d34684770cb4ea0cac1d22bd
T.kawabata <52056772+momorara@users.noreply.github.com>
2023-10-29 21:14:34 +0900

    Add files via upload

--- プログラム/lib_iR.py
+++ プログラム/lib_iR.py
@@ -12,35 +12,7 @@ ir_sensor_pin = Pin(10, Pin.IN)
 
 while True:
     if ir_sensor_pin.value() == 0:
-        print("物体が検出されました")
-    else:
-        print("物体は検出されていません")
-    utime.sleep(1)  # 1秒待つ
-
-
-# import machine
-# import time
-
-# tilt = machine.Pin(21, machine.Pin.IN, machine.Pin.PULL_UP)
-# led1 = machine.Pin(16, machine.Pin.OUT)
-# led2 = machine.Pin(17, machine.Pin.OUT)
-
-# def tilt_read():
-#     return tilt.value()
-
-# def main():
-
-#     while True:
-#         sense = tilt()
-#         if sense == 1:
-#             led1.on()
-#             led2.off()
-#         if sense == 0:
-#             led2.on()
-#             led1.off()           
-
-#         print(sense)
-#         time.sleep(0.1)
-
-# if __name__=='__main__':
-#     main()
+        print("信号が検出されました")
+    #else:
+    #    print("物体は検出されていません")
+    utime.sleep( 0.1)  # 1秒待つ
